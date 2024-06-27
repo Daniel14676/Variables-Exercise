0d1bfe189cb994e99fb3418f93303591aca8cd52
Daniel <danielm14676@gmail.com>
2024-06-27 10:03:14 -0500

    added saying with variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Merek";
+            int age = 123;
+            char rune = 'A';
+            bool open = true;
+            double width = 10.5;
+            decimal height = 20.5m;
+            Console.WriteLine($"Hello, {name}! He who is {age} old, and approached the door saying {rune} " +
+                $"as his password, which happens to be {open}ly the password. The door then opened, forming a hole " +
+                $"{width} by {height}");
         }
     }
 }
